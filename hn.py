1001fe468c8989c3c5d895513f23b3e8a6cf9341
Jeremy Kun <j2kun@users.noreply.github.com>
2024-08-07 14:39:51 -0700

    freshen up and adapt for my domain

--- hn.py
+++ hn.py
@@ -1,10 +1,12 @@
 import requests
 import indieweb_utils
 import sys
+import pprint
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-ME = "jamesg.blog"
+DOMAIN = "jeremykun.com"
+SEARCH_URL = f"https://hn.algolia.com/api/v1/search?query={DOMAIN}&tags=story&hitsPerPage=20"
 
 def get_post_text(post):
     if post.get("story_text"):
@@ -13,9 +15,10 @@ def get_post_text(post):
         return post["comment_text"]
     else:
         return ""
-    
+
+
 def send_webmention(post_url, target_url):
-    print(f"Found a link to {ME} on Hacker News!")
+    print(f"Found a link to {DOMAIN} on Hacker News!")
     print("Link to post: " + post_url)
     print("Link to my website: " + target_url)
 
@@ -24,40 +27,51 @@ def send_webmention(post_url, target_url):
     except indieweb_utils.webmentions.discovery.WebmentionEndpointNotFound:
         print("Webmention endpoint not found.")
         return
-    
+
     print("Webmention sent!")
+
 
 def main():
     try:
-        r = requests.get("https://hn.algolia.com/api/v1/search?query=jamesg.blog")
+        r = requests.get(SEARCH_URL)
     except requests.exceptions.RequestException as e:
         print(e)
         sys.exit(1)
 
-    hn_posts = r.json()["hits"]
+    response = r.json()
+    pprint.pp(response)
+    num_hits = response["nbHits"]
+    num_pages = response["nbPages"]
+    print(f"Found {num_hits} posts across {num_pages} paginated search pages.")
 
-    for post in hn_posts:
-        post_url = "https://news.ycombinator.com/item?id=" + str(post["objectID"])
+    for page in range(0, num_pages):
+        print(f"Querying page {page}")
+        try:
+            r = requests.get(f"{SEARCH_URL}&page={page}")
+        except requests.exceptions.RequestException as e:
+            print(e)
+            sys.exit(1)
 
-        post_http_url = post["url"]
-
-        if post_http_url is not None and urlparse(post_http_url).netloc == ME:
-            send_webmention(post_url, post_http_url)
-            continue
-
-        story_text = get_post_text(post)
-
-        content = BeautifulSoup(story_text, "html.parser")
-
-        links = content.find_all("a")
-
-        for link in links:
-            if link.get("href") is None:
+        response = r.json()
+        hn_posts = response["hits"]
+        for post in hn_posts:
+            post_url = "https://news.ycombinator.com/item?id=" + str(post["objectID"])
+            post_http_url = post.get("url")
+            if post_http_url is not None and urlparse(post_http_url).netloc == DOMAIN:
+                send_webmention(post_url, post_http_url)
                 continue
 
-            domain = urlparse(link.get("href")).netloc
+            story_text = get_post_text(post)
+            content = BeautifulSoup(story_text, "html.parser")
+            links = content.find_all("a")
+            for link in links:
+                if link.get("href") is None:
+                    continue
 
-            if domain == ME:
-                send_webmention(post_url, link.get("href"))
+                domain = urlparse(link.get("href")).netloc
+                if domain == DOMAIN:
+                    send_webmention(post_url, link.get("href"))
 
-main()
+
+if __name__ == "__main__":
+    main()
